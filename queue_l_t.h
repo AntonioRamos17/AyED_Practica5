f6a9b497f7663064996f85a84f3ea109c3e3e182
AntonioRamos17 <alu0101480367@ull.edu.es>
2022-05-01 20:20:44 +0000

    finalizado

--- queue_l_t.h
+++ queue_l_t.h
@@ -1,11 +1,18 @@
-// AUTOR: 
-// FECHA: 
-// EMAIL: 
-// VERSION: 2.0
-// ASIGNATURA: Algoritmos y Estructuras de Datos
-// PRÁCTICA Nº: 5
-// ESTILO: Google C++ Style Guide
-// COMENTARIOS: Clase TAD cola implementada con una lista
+/**
+ * FICHERO: queue_l_t.h
+ * AUTOR: Antonio Ramos Castilla (alu0101480367@ull.edu.es)
+ * PRACTICA: PRÁCTICA Nº: 5
+ * VERSION: 2.0
+ * FECHA: 2022-05-01
+ * ESTILO: Google C++ Style Guide
+ * ASIGNATURA: Algoritmos y Estructuras de Datos
+ * COMENTARIOS: Programa principal para probar la calculadora de expresiones 
+ *              en notaión polaca inversa (RPN, Reverse Polish Notation)
+ * COMPILACION: g++ main_rpn_t.cc -o main_rpn_t
+ * EJECUCION: ./main_rpn_t < data_rpn_t_1.txt
+ *            ./main_rpn_t < data_rpn_t_2.txt
+ *            ./main_rpn_t < data_rpn_t_3.txt
+ */
 
 #ifndef QUEUE_H_
 #define QUEUE_H_
@@ -19,14 +26,11 @@
 template <class T> class queue_l_t {
  private:
   dll_t<T>    l_;
-
  public:
   // constructor
   queue_l_t(void) : l_() {}
-
   // destructor
   ~queue_l_t(void) {}
-
   // operaciones
   bool empty(void) const;
   int size(void) const;
@@ -34,43 +38,75 @@ template <class T> class queue_l_t {
   void pop(void);
   const T& front(void) const;
   const T& back(void) const;
-
   // E/S
   std::ostream& write(std::ostream& os = std::cout) const;
 };
 
 
-// operaciones
+/**
+ * @brief : Comprueba si está vacío
+ * @param void
+ * @return : l_.empty
+ */
 template<class T> bool queue_l_t<T>::empty(void) const {
   return l_.empty();
 }
 
+/**
+ * @brief : Devuelve el tamaño de la cola
+ * @param void
+ * @return : l_.get_size()
+ */
 template<class T> int queue_l_t<T>::size(void) const {
   return l_.get_size();	
 }
 
+/**
+ * @brief : Extrae un dato en la cola
+ * @param dato
+ * @return : l_.push_front(node)
+ */
 template<class T> void queue_l_t<T>::push(const T& dato) {
   dll_node_t<T>* node = new dll_node_t<T>(dato);
   assert(node != NULL);
   l_.push_front(node);
 }
 
+/**
+ * @brief : Inserta un dato en la cola
+ * @param void
+ * @return : l_.pop_back()
+ */
 template<class T> void queue_l_t<T>::pop(void) {
   assert(!empty());
   delete l_.pop_back();
 }
 
+/**
+ * @brief : Getter del dato de la cola
+ * @param void
+ * @return : l_.get_tail()->get_data()
+ */
 template<class T> const T& queue_l_t<T>::front(void) const {
   assert(!empty());
   return (l_.get_tail()->get_data());
 }
 
+/**
+ * @brief : Getter del dato de la cabeza
+ * @param void
+ * @return : l_.get_head()->get_data()
+ */
 template<class T> const T& queue_l_t<T>::back(void) const {
   assert(!empty());
   return (l_.get_head()->get_data());
 }
 
- // E/S
+/**
+ * @brief : Método de escritura de la cola
+ * @param os
+ * @return : os
+ */
 template<class T> std::ostream& queue_l_t<T>::write(std::ostream& os) const {
   dll_node_t<T>* aux = l_.get_head();
   while (aux != NULL) {
@@ -81,8 +117,13 @@ template<class T> std::ostream& queue_l_t<T>::write(std::ostream& os) const {
   return os;
 }
 
-template<class T> std::ostream& operator<<(std::ostream& os,
-					   const queue_l_t<T>& q) {
+/**
+ * @brief : Sobrecarga del operador << 
+ * @param os
+ * @param q
+ * @return : os
+ */
+template<class T> std::ostream& operator<<(std::ostream& os, const queue_l_t<T>& q) {
   q.write(os);
   return os;
 }
--- stack_l_t.h
+++ stack_l_t.h
@@ -1,11 +1,18 @@
-// AUTOR: 
-// FECHA: 
-// EMAIL: 
-// VERSION: 2.0
-// ASIGNATURA: Algoritmos y Estructuras de Datos
-// TEMA: 5
-// ESTILO: Google C++ Style Guide
-// COMENTARIOS: 
+/**
+ * FICHERO: stack_l_t.h
+ * AUTOR: Antonio Ramos Castilla (alu0101480367@ull.edu.es)
+ * PRACTICA: PRÁCTICA Nº: 5
+ * VERSION: 2.0
+ * FECHA: 2022-05-01
+ * ESTILO: Google C++ Style Guide
+ * ASIGNATURA: Algoritmos y Estructuras de Datos
+ * COMENTARIOS: Programa principal para probar la calculadora de expresiones 
+ *              en notaión polaca inversa (RPN, Reverse Polish Notation)
+ * COMPILACION: g++ main_rpn_t.cc -o main_rpn_t
+ * EJECUCION: ./main_rpn_t < data_rpn_t_1.txt
+ *            ./main_rpn_t < data_rpn_t_2.txt
+ *            ./main_rpn_t < data_rpn_t_3.txt
+ */
 
 #ifndef STACKL_H_
 #define STACKL_H_
@@ -21,46 +28,65 @@ template<class T> class stack_l_t {
  public:	
   // constructor
   stack_l_t(void) : l_() {}
-
   // destructor
   ~stack_l_t(void) {}
-
   // operaciones
   void push(const T&);
   void pop(void);
   const T& top(void) const;
   bool empty(void) const;
-
   // E/S	
   std::ostream& write(std::ostream& os = std::cout) const;
-
-private:
+ private:
   dll_t<T> l_;
 };
 
 
-// operaciones
+/**
+ * @brief : Método para extraer de la pila
+ * @param dato
+ * @return : void
+ */
 template<class T> void stack_l_t<T>::push(const T& dato) {
   dll_node_t<T>* nodo = new dll_node_t<T>(dato);
   assert(nodo != NULL);
   l_.push_front(nodo);
 }
 
+/**
+ * @brief : Método para insertar en la pila
+ * @param void
+ * @return : void
+ */
 template<class T> void stack_l_t<T>::pop(void) {
   assert(!empty());
   delete l_.pop_front();
 }
 
+/**
+ * @brief : Getter del top de la pila
+ * @param void
+ * @return : l_.get_head()->get_data()
+ */
 template<class T> const T& stack_l_t<T>::top(void) const {
   assert(!empty());
   return l_.get_head()->get_data();
 }
 
+/**
+ * @brief : Comprueba si la pila está vacía
+ * @param void
+ * @return : l_.empty()
+ */
 template<class T> bool stack_l_t<T>::empty(void) const {
   return l_.empty();
 }
 
-// E/S
+/**
+ * @brief : Método de escritura de la pila
+ * @param os
+ * @return : os
+ */
 template<class T> std::ostream& stack_l_t<T>::write(std::ostream& os) const {
   dll_node_t<T>* aux = l_.get_head();
   while (aux != NULL) { 
